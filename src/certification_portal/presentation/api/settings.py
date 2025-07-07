229251e4f101c40d87c2f373ccd39a5d178193af
IlyaGrebenschikov <ilushagr22@gmail.com>
2025-07-07 10:28:28 +0400

    fix: server settings replaced and renamed

---
+++ src/certification_portal/presentation/api/settings.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class ServerSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix='SERVER_',
+        extra="ignore",
+    )
+
+    port: int = 8080
+    host: str = "0.0.0.0"
--- src/certification_portal/presentation/api/v1/settings.py
+++ src/certification_portal/presentation/api/v1/settings.py
@@ -1,23 +1,9 @@
 from pathlib import Path
-from typing import Optional
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class V1ApiSettings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        env_prefix='V1_API_',
-        extra="ignore",
-    )
-
-    port: int = 8080
-    host: str = "0.0.0.1"
-
+from pydantic_settings import BaseSettings
 
 class V1Settings(BaseSettings):
     root_dir: Path
-    api: V1ApiSettings
 
 
 def get_root_dir_path() -> Path:
